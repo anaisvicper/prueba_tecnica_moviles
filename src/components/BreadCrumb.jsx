1729f7fdd62c9bcc0e6491a4cba87bd7c23b08ee
Anais Vicper <anaisabel.vicianaperez@gmail.com>
2023-03-21 15:24:10 +0100

    feat: add BreadCrumb component

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -1,7 +1,24 @@
+import { NavLink, generatePath, useParams } from 'react-router-dom';
 import '../App.css';
+import useGetActiveRouteAndTree from '../hooks/useGetActiveRouteAndTree';
 
 const BreadCrumb = () => {
-  return <div>Productos</div>;
+  const { activeRouteTree } = useGetActiveRouteAndTree();
+  const params = useParams();
+  return activeRouteTree.map((route, index) => {
+    const isNotLast = index !== activeRouteTree.length - 1;
+    const routeParamValues = route.params.reduce(
+      (acc, routeParam) => ({ ...acc, [routeParam]: params[routeParam] }),
+      {}
+    );
+    const toPath = generatePath(route.path, routeParamValues);
+    return (
+      <NavLink key={route.path} to={toPath}>
+        <span>{route.name}</span>
+        {isNotLast ? <span> {' > '} </span> : ''}
+      </NavLink>
+    );
+  });
 };
 
 export default BreadCrumb;
